c9ded2a9be2eebc8713126e918b0c6b5792cee50
Meghan <Meghan>
2018-08-29 21:54:05 -0500

    Add additional news sources
    Add daily mail and BBC APIs

--- js/app.js
+++ js/app.js
@@ -15,6 +15,7 @@ firstDropDown.onclick = function() {
 
 
 	// connect to NYT
+
 	var xhttpNYT = new XMLHttpRequest();
 	
 	xhttpNYT.open('GET', 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=6a54f099ac5a4e3783ffa599adcf005e');
@@ -29,6 +30,7 @@ firstDropDown.onclick = function() {
 
 
 	// populate with NYT data
+
 	function nytContent () {
 
 	  var nytData = JSON.parse(xhttpNYT.responseText);
@@ -39,8 +41,15 @@ firstDropDown.onclick = function() {
 	  	img[i].src = nytData.results[i].multimedia[i].url;
 	  }
 
+
+		// pop-up code will live here
+
 		$( 'h3' ).click(function() {
-			alert( "this will be a popup" );
+			var	modal = document.querySelectorAll('#popUp .container');
+				container = document.getElementsByClassName('container');
+
+			alert('this should be a popup');
+
 		});
 
 	}
@@ -50,3 +59,86 @@ firstDropDown.onclick = function() {
 	nytContent();
 
 };
+
+// click for Daily Mail
+
+secondDropDown.onclick = function() {
+
+
+	// connect to DM
+
+	var xhttpDaily = new XMLHttpRequest();
+	
+	xhttpDaily.open('GET', 'https://newsapi.org/v2/top-headlines?country=us&apiKey=db4c258e05684c09b6aff90d7e0a3b1b');
+	xhttpDaily.send();
+
+	function errorHandler() {
+	  console.log('something went wrong');
+	}
+
+	xhttpDaily.onerror = errorHandler;
+	xhttpDaily.onload = nprContent;
+
+
+	// populate with DM data
+
+	function nprContent () {
+
+	  var dailyData = JSON.parse(xhttpDaily.responseText);
+	  console.log(dailyData);
+
+	  for (i = 0; i < headline.length; i++) {
+	  	headline[i].innerText = dailyData.articles[i].title;
+	  	category[i].innerText = dailyData.articles[i].author;
+	  	img[i].src = dailyData.articles[i].urlToImage;
+	  }
+
+	}
+
+
+	//run function
+	nprContent();
+
+};
+
+// click for BBC
+
+thirdDropDown.onclick = function() {
+
+
+	// connect to DM
+
+	var xhttpBBC = new XMLHttpRequest();
+	
+	xhttpBBC.open('GET', 'https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=db4c258e05684c09b6aff90d7e0a3b1b');
+	xhttpBBC.send();
+
+	function errorHandler() {
+	  console.log('something went wrong');
+	}
+
+	xhttpBBC.onerror = errorHandler;
+	xhttpBBC.onload = nprContent;
+
+
+	// populate with DM data
+
+	function nprContent () {
+
+	  var bbcData = JSON.parse(xhttpBBC.responseText);
+	  console.log(bbcData);
+
+	  for (i = 0; i < headline.length; i++) {
+	  	headline[i].innerText = bbcData.articles[i].title;
+	  	category[i].innerText = bbcData.articles[i].author;
+	  	img[i].src = bbcData.articles[i].urlToImage;
+	  }
+
+	}
+
+
+	//run function
+	nprContent();
+
+};
+
