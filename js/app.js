4abaa9390d9af7e863d8104b4954c38878a1bde2
Meghan <Meghan>
2018-08-29 19:49:01 -0500

    Populate page with NYT
    Add click functions to swap in NYT data

--- js/app.js
+++ js/app.js
@@ -3,28 +3,50 @@
 */
 var firstDropDown = document.querySelectorAll('li a')[1],
 	secondDropDown = document.querySelectorAll('li a')[2],
-	thirdDropDown = document.querySelectorAll('li a')[3];
+	thirdDropDown = document.querySelectorAll('li a')[3],
+	headline = document.querySelectorAll('h3'),
+	category = document.querySelectorAll('h6');
+	img = document.querySelectorAll('.featuredImage img');
 
-	console.log(firstDropDown);
+
+// click for NYT
+
+firstDropDown.onclick = function() {
 
 
-// connect to NYT
+	// connect to NYT
+	var xhttpNYT = new XMLHttpRequest();
+	
+	xhttpNYT.open('GET', 'https://api.nytimes.com/svc/topstories/v2/home.json?api-key=6a54f099ac5a4e3783ffa599adcf005e');
+	xhttpNYT.send();
 
-var xhttpNYT = new XMLHttpRequest();
-xhttpNYT.open('GET', 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=6a54f099ac5a4e3783ffa599adcf005e');
-xhttpNYT.send();
+	function errorHandler() {
+	  console.log('something went wrong');
+	}
 
-function errorHandler() {
-  console.log('something went wrong');
-}
+	xhttpNYT.onerror = errorHandler;
+	xhttpNYT.onload = nytContent;
 
-function successHandler () {
-  var nytData = JSON.parse(xhttpNYT.responseText);
-  firstDropDown.innerText = nytData.response.docs[0].source;
-  console.log(nytData);
-}
 
-xhttpNYT.onerror = errorHandler;
-xhttpNYT.onload = successHandler;
+	// populate with NYT data
+	function nytContent () {
 
-successhandler();
+	  var nytData = JSON.parse(xhttpNYT.responseText);
+
+	  for (i = 0; i < headline.length; i++) {
+	  	headline[i].innerText = nytData.results[i].title;
+	  	category[i].innerText = nytData.results[i].subsection;
+	  	img[i].src = nytData.results[i].multimedia[i].url;
+	  }
+
+		$( 'h3' ).click(function() {
+			alert( "this will be a popup" );
+		});
+
+	}
+
+
+	//run function
+	nytContent();
+
+};
